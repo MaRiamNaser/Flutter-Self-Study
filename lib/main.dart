daa7785be2d08ff0909b85b2a1fe1a1fb3b7e3b3
Mariam Nasser <mariamnasser979@gmail.com>
2021-04-08 01:43:30 -0700

    Added Example to show the lifecycle of widget.

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChangeNotifierProvider<MyProvider>(
-      
-          create: (_)=> MyProvider(),
-          child: CounterPro(),
-        ),
+      home: MyHomePage(), 
     );
   }
 }
 
-class CounterPro extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() { 
+    print("Init state is called.");
+    super.initState();
+    
+  }
+
+  @override
+  void didUpdateWidget( MyHomePage oldWidget) {
+    print("didUpdateWidget is called.");
+    super.didUpdateWidget(oldWidget);
+  }
+
+@override
+  void dispose() {
+    print("disposed is called.");
+    super.dispose();
+  }
+
+int _counter = 0;
   @override
   Widget build(BuildContext context) {
+    print("build is called!");
     return Scaffold(
       appBar: AppBar(title: Text('Demo')),
       body: Center(
-        child: Dashboard(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$_counter", 
+            style:TextStyle(fontSize: 45),),
+
+            const Text("Mariam"),// it is better to use const when this widget is not 
+                                // change through the app.
+           RaisedButton(
+             child: Text("Go to another Screen."),
+
+             onPressed: (){
+               // try push also to know when dispose function is called. ^^
+               Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (ctx){
+
+                      return Scaffold(
+                        appBar:AppBar(title: Text("Another")),
+                        body: null,
+                      );
+                  })
+               );
+             })
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
 
-          Provider.of<MyProvider>(context, listen: false).increment();
+          setState(() {
+            _counter++;
+          });
+
+          print("setState was called.");
         },
       ),
     );
